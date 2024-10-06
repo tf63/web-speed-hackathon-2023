296170eed0a7241668f43ebc0dd7deb478c884e9
tf63 <74246282+tf63@users.noreply.github.com>
2024-10-07 05:37:58 +0900

    feat: make icon support tree-shaking

--- src/client/components/foundation/Icon/Icon.tsx
+++ src/client/components/foundation/Icon/Icon.tsx
@@ -1,14 +1,26 @@
 import classNames from 'classnames';
 import type { FC } from 'react';
-import * as Icons from 'react-icons/fa';
+import type { IconType } from 'react-icons';
+import { FaArrowLeft, FaArrowRight, FaCheckCircle, FaPlay, FaShoppingCart, FaUser } from 'react-icons/fa';
 
 import * as styles from './Icon.styles';
 
+type IconName = 'FaArrowLeft' | 'FaArrowRight' | 'FaShoppingCart' | 'FaUser' | 'FaPlay' | 'FaCheckCircle';
+
 type Props = {
-  type: keyof typeof Icons;
+  type: IconName;
   width: number;
   height: number;
   color: string;
+};
+
+const Icons: { [key in IconName]: IconType } = {
+  FaArrowLeft: FaArrowLeft,
+  FaArrowRight: FaArrowRight,
+  FaCheckCircle: FaCheckCircle,
+  FaPlay: FaPlay,
+  FaShoppingCart: FaShoppingCart,
+  FaUser: FaUser,
 };
 
 export const Icon: FC<Props> = ({ color, height, type, width }) => {
